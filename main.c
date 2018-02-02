e48a5945db56d9d0a342b2fec6f120cee2727d70
David Koval <kovald@union.edu>
2018-02-01 19:47:31 -0500

    main
    set pins for both servo motors

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-/* 
- * File:   
- * Author: 
- * Date:
- * Purpose:
- * Modified:
+/*   
+ * David Koval, Desmond Tomaselli
+ * Professor Traver
+ * Project 2
+ * Feb. 5, 2017
  */
+
 /*********** COMPILER DIRECTIVES *********/
 
 // #include for textbook library header files
@@ -12,20 +12,31 @@
 
 // #defines for handy constants 
 #define LED LATAbits.LATA0  // LED on microstick is connected to RA0 (PORTA, bit 0)
-#define PWM_PERIOD 3125
-#define P_MIN 203
-#define P_MAX 266
+#define PWM_PERIOD 3125    //20ms PWM period with clock cycle of 6.4us from 1:256 prescale
+#define C_MIN 203          //1.3ms (min) pulse width for rotation servo
+#define C_MAX 266          //1.7ms (max) pulse width for rotation servo
+#define P_MIN 117          //0.75ms (min) pulse width for positional servo
+#define P_MAX 351          //2.25ms (max) pulse width for positional servo
 
 /*********** GLOBAL VARIABLE AND FUNCTION DEFINITIONS *******/
-uint16_t pulse_width;
+uint16_t pulse_width;       //pulse width for servo
 
 void configOC1() {
     T2CONbits.TON = 0; //Turns the timer off.
-    CONFIG_OC1_TO_RP(RB1_RP); //Maps the OC1 output to the remappable pin, RP1.
+    CONFIG_OC1_TO_RP(RB0_RP); //Maps the OC1 output to the remappable pin, RB1.
     OC1RS = 0; //Clears the register
     OC1R = 0; //Clears the register
     OC1CONbits.OCTSEL = 0; //Sets the output compare module to use Timer 2 as the clock source.
     OC1CONbits.OCM = 0b110; //Sets it to operate in PWM mode with fault pin disabled. 
+}
+
+void config0C2(void){
+    T2CONbits.TON = 0; //Turns the timer off.
+    CONFIG_OC2_TO_RP(RB4_RP); //Maps the OC2 output to the remappable pin, RB4.
+    OC2RS = 0;  // Clears OC2RS register
+    OC2R = 0;   // Clears OC2R register
+    OC2CONbits.OCTSEL = 0; //Sets the output compare module to use Timer 2 as the clock source.
+    OC2CONbits.OCM = 0b110; //Sets it to operate in PWM mode with fault pin disabled. 
 }
 
 void configTimer2() {
@@ -43,6 +54,25 @@ void _ISR _T2Interrupt(void) {
     _T2IF = 0; //Clears the Timer 2 interrupt flag, the last instruction in ISR
 }
 
+void continuousRatationServo(void){
+    DELAY_MS(500);
+        for (pulse_width=C_MIN; pulse_width < C_MAX; pulse_width++) {
+            DELAY_MS(500);
+        }
+        for (pulse_width=C_MAX; pulse_width > C_MIN; pulse_width--) {
+            DELAY_MS(500);
+        }
+}
+
+void positionalServo(void){
+    DELAY_MS(500);
+        for (pulse_width=P_MIN; pulse_width < P_MAX; pulse_width++) {
+            DELAY_MS(500);
+        }
+        for (pulse_width=P_MAX; pulse_width > P_MIN; pulse_width--) {
+            DELAY_MS(500);
+        }
+}
 /********** MAIN PROGRAM LOOP********************************/
 int main ( void )  //main function that....
 { 
@@ -53,6 +83,7 @@ int main ( void )  //main function that....
 	configClock();  //Sets the clock to 40MHz using FRC and PLL
   	configHeartbeat(); //Blinks the LED on RA1
     configOC1(); //Configures the register
+    config0C2(); //Configures the reg
     configTimer2(); //Configures the timer
     
 
@@ -62,13 +93,8 @@ int main ( void )  //main function that....
 
 /* Main program loop */
     while (1) {	
-		
+        continuousRatationServo();
+//        positionalServo();
     }
-        DELAY_MS(500);
-        for (pulse_width=P_MIN; pulse_width < P_MAX; pulse_width++) {
-            DELAY_MS(500);
-        }
-        for (pulse_width=P_MAX; pulse_width > P_MIN; pulse_width--) {
-            DELAY_MS(500);
-        }
+        
 }
